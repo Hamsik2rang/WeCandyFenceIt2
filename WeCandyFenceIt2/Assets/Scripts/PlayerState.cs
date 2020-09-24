aa4f98db80d385f3e4ff3245f148a42ec0fcd5a5
KimSoo-hwan <56312512+KimSoo-hwan@users.noreply.github.com>
2020-09-24 19:34:03 +0900

    [#3] Scene 심플하게 생성
    유니티 프로젝트 생성 및
    러프하게 Scene 구현

---
+++ WeCandyFenceIt2/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerState : MonoBehaviour
+{
+    private float timer;
+    private int waitingTime;
+    bool isAlive;
+
+    void Start()
+    {
+        isAlive = true;
+        timer = 0.0f;
+        waitingTime = 5;
+    }
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // 5초 뒤 Player 사망
+        if (timer > waitingTime)
+        {
+            isAlive = false;
+            timer = 0.0f;
+        }
+
+        if (!isAlive)
+        {
+            GameOver();
+        }
+    }
+
+    // 게임오버 시 ResultScene 불러오기
+    void GameOver()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+}
---
+++ WeCandyFenceIt2/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void LoadResultScene()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+
+}
