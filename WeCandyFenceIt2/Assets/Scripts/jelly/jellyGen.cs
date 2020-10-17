94266dce6a01a046a428d40c72da81d35c7b6b8d
won mincheol <59256157+wonmincheol@users.noreply.github.com>
2020-10-18 02:25:23 +0900

    [#13]  젤리 낙하, 생성
    낙하 생성 추가
    낙하속도 랜덤, 생성시 상단에 랜덤 생성
    젤리가 카메라 벗어날시 파괴

---
+++ WeCandyFenceIt2/Assets/Scripts/jelly/jellyGen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jellyGen : MonoBehaviour
+{
+
+    public GameObject jelly;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("jellyGenCoroutine");
+        StartCoroutine("jellyGenStopCoroutine");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator jellyGenCoroutine()
+    {
+        float[] genAreaX = { -6f, 6f };
+        float genAreaY = 5f;
+        float delayTimes = 0.99f;
+        float genDelay = 2f;
+        while (true)
+        {
+            Instantiate(jelly, new Vector2(UnityEngine.Random.Range(genAreaX[0], genAreaX[1]), genAreaY), Quaternion.identity);
+            yield return new WaitForSeconds(genDelay);
+            genDelay = genDelay * delayTimes;
+        }
+    }
+    IEnumerable jellyGenStopCoroutine()
+    {
+        yield return new WaitForSeconds(3600f);
+        StopCoroutine("jellyGenCoroutine");
+        StopCoroutine("jellyGenStopCoroutine");
+    }
+}
---
+++ WeCandyFenceIt2/Assets/Scripts/jelly/jellyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jellyMove : MonoBehaviour
+{
+    float fallSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fallSpeed = UnityEngine.Random.Range(0.1f, 3);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0, -fallSpeed) * Time.deltaTime);
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
