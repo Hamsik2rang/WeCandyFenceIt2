da25e69567f7ba0f6f6a76d3d4a3cc1a82dea769
won mincheol <59256157+wonmincheol@users.noreply.github.com>
2020-11-11 00:13:38 +0900

    [#11] 폭탄 구현(스크립트 포함)
    애니메이션, 스코어 제외
    폭탄생성 딜레이, 폭탄범위, 폭탄 버튼, 폭탄 충돌판정, 충돌시 디스트로이

---
+++ WeCandyFenceIt2/Assets/Scripts/Bomb/BombExplosion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+    float BombWaitTime = 2f;
+    float BombExplosionRadius = 2f;
+    CircleCollider2D BombExplosionAreaCollider;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BombExplosionAreaCollider = GetComponent<CircleCollider2D>();
+        BombExplosionAreaCollider.enabled=false;
+        BombExplosionAreaCollider.radius = BombExplosionRadius;
+        StartCoroutine("BombExplosionCoroutine");
+        Debug.Log("bombset");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator BombExplosionCoroutine()
+    {
+        yield return new WaitForSeconds(BombWaitTime);
+        BombExplosionAreaCollider.enabled = true;
+        Debug.Log("bomb");
+        //애니메이션 추가
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("input");
+
+        if (collision.gameObject.tag == "Jelly")
+        {
+            Destroy(collision.gameObject);
+            //스코어 추가
+        }
+    }
+}
---
+++ WeCandyFenceIt2/Assets/Scripts/Bomb/BombSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawn : MonoBehaviour
+{
+    public GameObject BombOrigin;
+    float BombSpawnDelay = 10f;
+    bool BombSpawnCoroutineBool = false;
+    GameObject Player;
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnClickBombSpawn()
+    {
+        if (BombSpawnCoroutineBool == false)
+        {
+            StartCoroutine("BombSpawnCoroutine");
+        }
+    }
+    IEnumerator BombSpawnCoroutine()
+    {
+        BombSpawnCoroutineBool = true;
+        Instantiate(BombOrigin, new Vector2(Player.transform.position.x, Player.transform.position.y), Quaternion.identity);
+        yield return new WaitForSeconds(10f);
+        BombSpawnCoroutineBool = false;
+    }
+}
