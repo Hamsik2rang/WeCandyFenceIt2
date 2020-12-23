a4444d63f35022ed6c3d03d5902c924dcb19a827
KimSoo-hwan <56312512+KimSoo-hwan@users.noreply.github.com>
2020-12-23 10:04:42 +0900

    [#20] 배경화면 생성
    배경화면을 생성하고 삭제하는 코드 추가

---
+++ WeCandyFenceIt2/Assets/Scripts/BackgroundDestroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundDestroy : MonoBehaviour
+{
+    static float BG_SIZE = 30.0f;
+
+    BackgroundGen backgroundGen;
+    GameObject player;
+    Vector2 bgPos;
+    float xDiff, yDiff;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundGen = GameObject.Find("BackgroundGenerator").GetComponent<BackgroundGen>();
+        player = GameObject.Find("Player");
+        bgPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(BgDestroyCoroutine());
+    }
+
+    IEnumerator BgDestroyCoroutine()
+    {
+        xDiff = Mathf.Abs(player.transform.position.x - gameObject.transform.position.x);
+        yDiff = Mathf.Abs(player.transform.position.y - gameObject.transform.position.y);
+
+        if (xDiff >= 2 * BG_SIZE || yDiff >= 2 * BG_SIZE)
+        {
+            backgroundGen.bgList.Remove(new Vector2(bgPos.x / BG_SIZE, bgPos.y / BG_SIZE));
+            Destroy(gameObject);
+        }
+
+        yield return new WaitForSeconds(0.2f);
+    }
+}
---
+++ WeCandyFenceIt2/Assets/Scripts/BackgroundGen.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundGen : MonoBehaviour
+{
+    static float BG_SIZE = 30.0f;
+
+    public GameObject background;
+    GameObject player;
+
+    public LinkedList<Vector2> bgList = new LinkedList<Vector2>();
+
+    float bgGenDelay = 0.2f;
+    Vector3 playerPos;
+    Vector2 playerGrid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Instantiate(background, new Vector3(0, 0, 10), Quaternion.identity);
+        bgList.AddLast(new Vector2(0, 0));
+        StartCoroutine(BgGenCoroutine());
+    }
+
+    IEnumerator BgGenCoroutine()
+    {
+        while (true)
+        {
+            bool isOnLeft, isOnRight, isOnTop, isOnBottom;
+            bool isAlreadyLeft, isAlreadyRight, isAlreadyTop, isAlreadyBottom;
+            bool isAlreadyTopLeft, isAlreadyTopRight, isAlreadyBottomLeft, isAlreadyBottomRight;
+
+            isOnLeft = isOnRight = isOnTop = isOnBottom = false;
+            isAlreadyLeft = isAlreadyRight = isAlreadyTop = isAlreadyBottom = false;
+            isAlreadyTopLeft = isAlreadyTopRight = isAlreadyBottomLeft = isAlreadyBottomRight = false;
+
+            playerPos = player.transform.position;
+            float gridX = (int)playerPos.x / ((int)BG_SIZE / 2) - (int)playerPos.x / (int)BG_SIZE;
+            float gridY = (int)playerPos.y / ((int)BG_SIZE / 2) - (int)playerPos.y / (int)BG_SIZE;
+            playerGrid = new Vector2(gridX, gridY);
+            
+            if (playerPos.x <= playerGrid.x * BG_SIZE - (BG_SIZE / 6))
+                isOnLeft = true;
+            if (playerPos.x >= playerGrid.x * BG_SIZE + (BG_SIZE / 6))
+                isOnRight = true;
+            if (playerPos.y <= playerGrid.y * BG_SIZE - (BG_SIZE / 6))
+                isOnBottom = true;
+            if (playerPos.y >= playerGrid.y * BG_SIZE + (BG_SIZE / 6))
+                isOnTop = true;
+
+            foreach (var bgGrid in bgList)
+            {
+                if (bgGrid.x == playerGrid.x - 1 && bgGrid.y == playerGrid.y)
+                    isAlreadyLeft = true;
+                if (bgGrid.x == playerGrid.x + 1 && bgGrid.y == playerGrid.y)
+                    isAlreadyRight = true;
+                if (bgGrid.x == playerGrid.x && bgGrid.y == playerGrid.y - 1)
+                    isAlreadyBottom = true;
+                if (bgGrid.x == playerGrid.x && bgGrid.y == playerGrid.y + 1)
+                    isAlreadyTop = true;
+                if (bgGrid.x == playerGrid.x - 1 && bgGrid.y == playerGrid.y + 1)
+                    isAlreadyTopLeft = true;
+                if (bgGrid.x == playerGrid.x + 1 && bgGrid.y == playerGrid.y + 1)
+                    isAlreadyTopRight = true;
+                if (bgGrid.x == playerGrid.x - 1 && bgGrid.y == playerGrid.y - 1)
+                    isAlreadyBottomLeft = true;
+                if (bgGrid.x == playerGrid.x + 1 && bgGrid.y == playerGrid.y - 1)
+                    isAlreadyBottomRight = true;
+            }
+
+            if (isOnLeft && !isAlreadyLeft)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x - 1, playerGrid.y));
+                Instantiate(background, new Vector3((playerGrid.x - 1) * BG_SIZE, playerGrid.y * BG_SIZE, 10), Quaternion.identity);
+            }
+            if(isOnRight && !isAlreadyRight)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x + 1, playerGrid.y));
+                Instantiate(background, new Vector3((playerGrid.x + 1) * BG_SIZE, playerGrid.y * BG_SIZE, 10), Quaternion.identity);
+            }
+            if(isOnBottom && !isAlreadyBottom)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x, playerGrid.y - 1));
+                Instantiate(background, new Vector3(playerGrid.x * BG_SIZE, (playerGrid.y - 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+            if(isOnTop && !isAlreadyTop)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x, playerGrid.y + 1));
+                Instantiate(background, new Vector3(playerGrid.x * BG_SIZE, (playerGrid.y + 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+            if(isOnTop && isOnLeft && !isAlreadyTopLeft)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x - 1, playerGrid.y + 1));
+                Instantiate(background, new Vector3((playerGrid.x - 1) * BG_SIZE, (playerGrid.y + 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+            if(isOnTop && isOnRight && !isAlreadyTopRight)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x + 1, playerGrid.y + 1));
+                Instantiate(background, new Vector3((playerGrid.x + 1) * BG_SIZE, (playerGrid.y + 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+            if (isOnBottom && isOnLeft && !isAlreadyBottomLeft)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x - 1, playerGrid.y - 1));
+                Instantiate(background, new Vector3((playerGrid.x - 1) * BG_SIZE, (playerGrid.y - 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+            if (isOnBottom && isOnRight && !isAlreadyBottomRight)
+            {
+                bgList.AddLast(new Vector2(playerGrid.x + 1, playerGrid.y - 1));
+                Instantiate(background, new Vector3((playerGrid.x + 1) * BG_SIZE, (playerGrid.y - 1) * BG_SIZE, 10), Quaternion.identity);
+            }
+
+            yield return new WaitForSeconds(bgGenDelay);
+        }
+    }
+}
